9c0fc0018499d9e544e7e8c5f2eb670b65cecc22
junaidumer79 <junaidumer79@gmail.com>
2016-07-31 13:16:06 +0500

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO , WORLD ! ");
+            Console.WriteLine("HELLO , WORLD ! changes are done baby on the server");
         }
     }
 }
